3b35d1ba2a623f0837dc5cbacd1bc3cc1624d5da
akshatkushwaha <akshat.official999@gmail.com>
2023-06-28 04:13:50 +0530

    StudentCreditMapping.java was creating problem: Hence removed

--- src/main/java/com/akshat/project/university/management/system/repository/StudentCreditMappingRepository.java
+++
@@ -1,7 +0,0 @@
-package com.akshat.project.university.management.system.repository;
-
-import com.akshat.project.university.management.system.model.StudentCreditMapping;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentCreditMappingRepository extends JpaRepository<StudentCreditMapping, Long> {
-}
